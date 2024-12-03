7336dfdc3539481a8d45fb1388c89e75facd0039
unknown <nandan6102003@gmail.com>
2024-12-03 17:43:44 +0530

    profile page updated

--- Frontend/src/components/rightbar/Rightbar.jsx
+++ Frontend/src/components/rightbar/Rightbar.jsx
@@ -3,22 +3,105 @@ import "./Rightbar.css"
 import gift from '../../assets/gift.png'
 import adds from "../../assets/ad.png"
 import Online from '../online/Online'
-import {Users}from "../../components/post/dummyData"
+import friend1 from "../../assets/person/1.jpeg"
+import friend2 from "../../assets/person/2.jpeg"
+import friend3 from "../../assets/person/3.jpeg"
+import friend4 from "../../assets/person/4.jpeg"
+import friend5 from "../../assets/person/5.jpeg"
+import friend6 from "../../assets/person/6.jpeg"
+
+
+
+import { Users } from "../../components/post/dummyData"
 
 
 export default function Rightbar() {
-  return (
-    <div className='right'>
-      <div className="right-bar-wrapper">
+
+
+  const HomePage = () => {
+    return (
+      <>
         <div className="birthday-container">
           <img src={gift} alt="" className="birth-day-img" />
-          <span className="birth-day-text"> <b>nandan</b> and <b>3 ather friends</b>  have a birthday</span>
+          <span className="birth-day-text"> <b>naman</b> and <b>3 ather friends</b>  have a birthday</span>
         </div>
         <img src={adds} alt="" className="right-bar-adds" />
         <h4 className="right-bar-titles">Online friends</h4>
         <ul className="right-bar-friends-list">
-         {Users.map(u=>(<Online key={u.id} user = {u}/>))}
+          {Users.map(u => (<Online key={u.id} user={u} />))}
         </ul>
+      </>
+    )
+  }
+
+  const Profile = () => {
+    return (
+      <>
+        <h2 className="user-info-title">User information</h2>
+        <div className="right-bar-info">
+          <div className="right-bar-info-item">
+          <span className="right-bar-info-key">City :</span>
+          <span className="right-bar-info-value">Bangaluru</span>
+        </div>
+        <div className="right-bar-info-item">
+          <span className="right-bar-info-key">From :</span>
+          <span className="right-bar-info-value">j p nagar</span>
+        </div>
+        <div className="right-bar-info-item">
+          <span className="right-bar-info-key">Relationship:</span>
+          <span className="right-bar-info-value">single</span>
+        </div>
+        </div>
+
+        <h2 className="user-info-title">User friends</h2>
+      <div className="right-bar-followings">
+        <div className="right-bar-following">
+          <img src={friend1} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+
+        <div className="right-bar-following">
+          <img src={friend1} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+        <div className="right-bar-following">
+          <img src={friend1} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+
+        <div className="right-bar-following">
+          <img src={friend2} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+
+        <div className="right-bar-following">
+          <img src={friend3} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+
+        <div className="right-bar-following">
+          <img src={friend4} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+
+        <div className="right-bar-following">
+          <img src={friend5} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+
+        <div className="right-bar-following">
+          <img src={friend6} alt="" className="right-bar-following-img" />
+          <span className="right-bar-following-name">naman</span>
+        </div>
+      </div>
+      </>
+    )
+  }
+  return (
+    <div className='right'>
+      <div className="right-bar-wrapper">
+        <Profile />
+        {/* <HomePage/> */}
       </div>
     </div>
   )
---
+++ Frontend/src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Login.css"
+
+export default function Login() {
+  return (
+    <div className='login'>
+      <div className="login-wrapper">
+        <div className="login-left">
+            <h3 className="login-logo">ReadNote</h3>
+            <span className="login-desc">dfjdngkdsngksdngfg dgsdkngdsk</span>
+        </div>
+        <div className="login-right">
+            <div className="login-box"> </div>
+        </div>
+      </div>
+    </div>
+  )
+}
