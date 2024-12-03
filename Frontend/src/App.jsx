d2abe599fefd40f652aa939230eb3bf951232ef5
unknown <nandan6102003@gmail.com>
2024-12-03 16:36:48 +0530

    profile picture updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import Profile from './pages/profile/Profile'
 
 export default function App() {
   return (
     <div className='app'>
-       <Home/>
+       {/* <Home/> */}
+       <Profile/>
     </div>
   )
 }
--- Frontend/src/pages/profile/Profile.jsx
+++ Frontend/src/pages/profile/Profile.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import './Profile.jsx'
+import Navbar from '../../components/navbar/Navbar'
+import Leftbar from '../../components/leftbar/Leftbar'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+import './Profile.css'
+import coverImg from "../../assets/post/3.jpeg"
+import profile from "../../assets/person/1.jpeg"
 
 export default function Profile() {
   return (
     <div>
-      
+      <Navbar />
+      <div className="profile">
+        <Leftbar />
+        <div className="profile-right">
+          <div className="profile-right-top">
+            <div className="profile-cover">
+              <img src={coverImg} alt="" className="profile-cover-img" />
+              <img src={profile} alt="" className="profile-user-img" />
+            </div>
+            <div className="profile-info">
+              <h4 className="profile-info-Name">Saried rteagerth</h4>
+              <span className="profile-info-desc">hello wewjnk ertnek dnfkldn</span>
+            </div>
+          </div>
+          <div className="profile-right-bottom">
+            <Feed />
+            <Rightbar />
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
